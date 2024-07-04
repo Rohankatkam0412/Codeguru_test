af363421aa53a5c947aae8de2346f4dc58800110
monarch-rohankatkam <rkatakam@monarchtractor.com>
2024-07-04 16:42:49 +0530

    Introduce insecure function for testing CodeGuru

--- src/main.py
+++ src/main.py
@@ -4,6 +4,12 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def insecure_function():
+    secret = "my_secret_password"
+    print(secret)
+
 if __name__ == "__main__":
     print(f"5 + 3 = {add(5, 3)}")
     print(f"5 - 3 = {subtract(5, 3)}")
+    insecure_function()
+
